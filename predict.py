bc5559b7cd237559f93576e6da80cc4016d103dd
a7med12345 <39575313+a7med12345@users.noreply.github.com>
2018-07-12 18:43:35 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,1 +1,14 @@
 
+from functions import*
+import argparse
+import torch
+
+parser = argparse.ArgumentParser(description='PyTorch flower classification')
+
+parser.add_argument('checkpoint')
+
+parser.add_argument('image_path')
+
+parser.add_argument('--topk', type=int)
+
+parser.add_argument('--gpu',action='store_true')
