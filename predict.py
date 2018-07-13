5be65e0b72a738edcdb27afc0dde6e9509b1cf29
a7med12345 <39575313+a7med12345@users.noreply.github.com>
2018-07-13 11:49:04 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,10 +5,28 @@ import torch
 
 parser = argparse.ArgumentParser(description='PyTorch flower classification')
 
-parser.add_argument('checkpoint')
-
 parser.add_argument('image_path')
+parser.add_argument('checkpoint')
 
 parser.add_argument('--topk', type=int)
 
 parser.add_argument('--gpu',action='store_true')
+parser.add_argument('--category_names')
+
+
+def main():
+  args = parser.parse_args()
+    if(args.gpu):
+        if(torch.cuda.is_available()):
+            device = torch.device("cuda:0")
+        else:
+            print("GPU not available")
+            return
+    else:
+        device = torch.device("cpu")
+   
+  model,class_to_idx = load_checkpoint(args.checkpoint)
+  print(predict(image_path, model, device,class_to_idx))
+    
+ if __name__ == "__main__":
+    main()  
