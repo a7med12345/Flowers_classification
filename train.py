0decc8833e670f2d074997cb4d8bdc0029e913f7
a7med12345 <39575313+a7med12345@users.noreply.github.com>
2018-07-13 19:42:47 -0400

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from functions import*
 import argparse
 import torch
 
+
 parser = argparse.ArgumentParser(description='PyTorch flower classification')
 
 parser.add_argument('--epochs', default=3, type=int,help='number of total epochs to run')
@@ -10,13 +11,13 @@ parser.add_argument('--learning_rate', default=0.001, type=float, help='initial 
 
 parser.add_argument('--hidden_units', type=int,default=1024, help='number of hidden units in the classifier')
 
-parser.add_argument('--save-dir', dest='save_dir', help='The directory used to save the trained models',default='/', type=str)
+parser.add_argument('--save_dir', dest='save_dir', help='The directory used to save the trained models',default='saved.pkl', type=str)
 
-parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet101',)
+parser.add_argument('--arch', default='resnet101',type=str)
 
-parser.add_argument('--data_dir', '-a', metavar='ARCH', default='flowers')
+parser.add_argument('data_dir')
 
-parser.add_argument('--gpu')
+parser.add_argument('--gpu',action='store_true')
 
 
 
@@ -27,22 +28,23 @@ def main():
     #building the model
     args = parser.parse_args()
     if(args.gpu):
-        if torch.cuda.is_available():
+        if(torch.cuda.is_available()):
             device = torch.device("cuda:0")
         else:
-            print("GPU not available)
+            print("GPU not available")
             return
-     else:
+    else:
         device = torch.device("cpu")
                   
-    model = building_model(args.hidden_units,args.arch)
+    model,optimizer = building_model(args.hidden_units,args.arch,args.learning_rate)
     
     #Preparing data
     train_datasets,trainloader,validloader,testloader = data_preparation(args.data_dir)
     
     #training and testing the model
     
-    train(model, trainloader,validloader,testloader,args.learning_rate,device,args.epochs)
-    
-if __name__ == '__main__':
-    main()
+    train(model, train_datasets,trainloader,validloader,testloader,optimizer,device,args.epochs,args.hidden_units,\
+          args.arch,args.learning_rate,args.save_dir)
+
+if __name__ == "__main__":
+    main()  
